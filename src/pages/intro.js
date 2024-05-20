de4d7ebf2a0bf3f61c7e5f8566d8e85d883524a4
Johanes Simarmata <johanes.s@tokopedia.com>
2024-05-20 22:21:03 +0700

    add 22.30

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -91,11 +91,11 @@ const IntroPage = () => {
 
     useEffect(() => {
         let deadline = new Date();
-        deadline.setDate(23)
+        deadline.setDate(20)
         deadline.setMonth(4)
         deadline.setFullYear(2024)
-        deadline.setHours(0)
-        deadline.setMinutes(0)
+        deadline.setHours(22)
+        deadline.setMinutes(30)
         deadline.setSeconds(0)
         clearTimer(deadline);
         // eslint-disable-next-line
@@ -112,7 +112,7 @@ const IntroPage = () => {
     }
 
     const handleOpenModalPassword = () => {
-        const expectedTime = new Date(2024, 4, 23, 0, 0, 0, 0).getTime()
+        const expectedTime = new Date(2024, 4, 20, 22, 30, 0, 0).getTime()
         const currentTime = Date.now()
 
         if (expectedTime <= currentTime) {
