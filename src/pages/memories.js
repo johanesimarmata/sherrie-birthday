84b67c8fb5fc65ae0cd8629b915c7a9bc8efd22a
Johanes Simarmata <johanes.s@tokopedia.com>
2024-05-22 22:58:12 +0700

    update memories

--- src/pages/memories.js
+++ src/pages/memories.js
@@ -28,7 +28,6 @@ const MemoriesPage = () => {
                     onStoryEnd={(s, st) => console.log("story ended", s, st)}
                     onAllStoriesEnd={(s, st) => console.log("all stories ended", s, st)}
                     onStoryStart={(s, st) => console.log("story started", s, st)}
-                    onNext={() => console.log("next button pressed")}
                     onPrevious={() => console.log("previous button pressed")}
                     storyContainerStyles={{ borderRadius: 8, overflow: "hidden" }}
                     height="100%"
@@ -39,33 +38,54 @@ const MemoriesPage = () => {
     )
 }
 
-const Story2 = ({ action, isPaused }) => {
-    return (
-      <div style={{ ...contentStyle, background: "Aquamarine", color: "#333" }}>
-        <h1>You get the control of the story.</h1>
-        <p>
-          Render your custom JSX by passing just a{" "}
-          <code style={{ fontStyle: "italic" }}>content</code> property inside
-          your story object.
-        </p>
-        <p>
-          You get a <code style={{ fontStyle: "italic" }}>action</code> prop as an
-          input to your content function, that can be used to play or pause the
-          story.
-        </p>
-        <h1>{isPaused ? "Paused" : "Playing"}</h1>
-        <h4>v2 is out 🎉</h4>
-        <p>React Native version coming soon.</p>
-      </div>
-    );
-  };
+// const Story2 = ({ action, isPaused }) => {
+//     return (
+//       <div style={{ ...contentStyle, background: "Aquamarine", color: "#333" }}>
+//         <h1>You get the control of the story.</h1>
+//         <p>
+//           Render your custom JSX by passing just a{" "}
+//           <code style={{ fontStyle: "italic" }}>content</code> property inside
+//           your story object.
+//         </p>
+//         <p>
+//           You get a <code style={{ fontStyle: "italic" }}>action</code> prop as an
+//           input to your content function, that can be used to play or pause the
+//           story.
+//         </p>
+//         <h1>{isPaused ? "Paused" : "Playing"}</h1>
+//         <h4>v2 is out 🎉</h4>
+//         <p>React Native version coming soon.</p>
+//       </div>
+//     );
+//   };
 // https://github.com/mohitk05/react-insta-stories/blob/master/example/src/App.js
 // https://mohitk05.github.io/react-insta-stories/
 
 const stories2 = [
     {
-        url: Sherrie1,
-        type: "video",
+      content: () => {
+        return(
+          <div style={{
+            background: "black", 
+            height: "100%", 
+            width: "100%",
+            color: "white", 
+            justifyContent: "center", 
+            alignItems: "center", 
+            display: "flex",
+            fontSize: "24px",
+            fontFamily: "monospace",
+            textAlign: "center",
+            padding: "30px"
+          }}>
+            <h1>Here's a little memory of all the moments we've shared together 🥳 🎉 🎁</h1>
+          </div>
+        )
+      }
+    },
+    {
+      url: Sherrie1,
+      type: "video",
     },
     {
       url: Sherrie2,
@@ -78,6 +98,27 @@ const stories2 = [
     {
       url: Sherrie4,
       type: "video",
+    },
+    {
+      content: () => {
+        return(
+          <div style={{
+            background: "black", 
+            height: "100%", 
+            width: "100%",
+            color: "white", 
+            justifyContent: "center", 
+            alignItems: "center", 
+            display: "flex",
+            fontSize: "24px",
+            fontFamily: "monospace",
+            textAlign: "center",
+            padding: "30px"
+          }}>
+            <h1>Now, let's see our rundown for today. May today be the most special day for you 🥳 🎉 🎊 🎂</h1>
+          </div>
+        )
+      }
     },
     {
       url: Invitation1,
@@ -167,9 +208,9 @@ const stories2 = [
     //     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
     //   type: "video",
     // },
-    {
-      content: Story2,
-    },
+    // {
+      // content: Story2,
+    // },
     // {
     //   url: "https://plus.unsplash.com/premium_photo-1676231417481-5eae894e7f68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80",
     // },
@@ -206,13 +247,13 @@ const stories2 = [
 //     color: "#333",
 // };
   
-const contentStyle = {
-    background: "#333",
-    width: "100%",
-    padding: 20,
-    color: "white",
-    height: "100%",
-};
+// const contentStyle = {
+//     background: "#333",
+//     width: "100%",
+//     padding: 20,
+//     color: "white",
+//     height: "100%",
+// };
   
 // const customSeeMore = {
 //     textAlign: "center",
